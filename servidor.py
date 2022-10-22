c627b58ec3c40a8654e39dbae18ad13b6b570760
Lucas Gabriel <marinholgabriel@gmail.com>
2022-10-21 21:53:14 -0300

    Ajustando operação de informar venda

--- servidor.py
+++ servidor.py
@@ -15,7 +15,7 @@ class Venda():
         return self.nomeVendedor + " " + self.idLoja + " " + str(self.dataVenda) + " " + str(self.valorVenda)
 
 def main():
-    print("\nIniciando servidor")
+    print("\nIniciando servidor.")
 
     HOST = "localhost"
     PORT = 9000
@@ -24,51 +24,56 @@ def main():
     try:
         servidor.bind((HOST, PORT))
         servidor.listen()
+        print("\nServidor iniciado.")
     except:
         print("\n Não foi possível iniciar o servidor")
 
+    i = 0
     while True:
+        i += 1
         cliente, enderecoCliente = servidor.accept()
         tipoCliente = cliente.recv(1024).decode("utf-8")
-        print(f"Cliente: {cliente} conectou.")
-        iniciarVendedorOuGerente(tipoCliente, cliente)
+        print(f"Quantidade de clientes que já se conectou nesse servidor: {i}.")
+        threadVendedor = threading.Thread(target=iniciarVendedorOuGerente, args=[tipoCliente, cliente])
+        threadVendedor.start()
 
-        
 def iniciarVendedorOuGerente(tipoCliente, cliente):
     if tipoCliente == "Vendedor":
-        threadVendedor = threading.Thread(target=operacoesVendedor, args=[cliente])
-        threadVendedor.start()
-        print(f"Cliente: {cliente} é um vendedor.")
+        operacoesVendedor(cliente)
+        print(f"Cliente: {cliente} é um vendedor. Sua thread é: {threading.enumerate()}")
     elif tipoCliente == "Gerente":
         operacoesGerente(cliente)
-        print(f"Cliente: {cliente} é um gerente.")
+        print(f"Cliente: {cliente} é um gerente. Sua thread é: {threading.enumerate()}")
     else:
-        cliente.sendall("Você não tem permissão para se conectar".encode("utf-08"))
-        print("Você não tem permissão para se conectar.")
+        print("Cliente não é um vendedor ou gerente.")
     
 def operacoesVendedor(cliente):
     global vendasRealizadas
     codigoOperacao = cliente.recv(1024).decode("utf-8")
     if codigoOperacao == "OP001":
-        print(f"Cliente: {cliente} escolheu OP001.")
+        cliente.sendall("OK_OP".encode("utf-8"))
+        print(f"Cliente: {cliente} escolheu OP001.") 
         try:
             nomeVendedor = cliente.recv(2048).decode("utf-8")
             idLoja = cliente.recv(2048).decode("utf-8")
             dataVenda = cliente.recv(2048).decode("utf-8")
             formatarEmData = datetime.date(int(dataVenda[6:10]),int(dataVenda[3:5]),int(dataVenda[0:2]))
-            valorVenda = float(cliente.recv(2048).decode("utf-8"))
-
-            vendasRealizadas.append(Venda(nomeVendedor, idLoja, formatarEmData, valorVenda))
             cliente.sendall("OK".encode("utf-8"))
 
-            print(vendasRealizadas[0].valoresObj())
+            valorVenda = float(cliente.recv(2048).decode("utf-8"))
+            cliente.sendall("OK".encode("utf-8"))
+
+            vendasRealizadas.append(Venda(nomeVendedor, idLoja, formatarEmData, valorVenda))
+
+            for vendas in vendasRealizadas:
+                print(vendas.valoresObj())
             iniciarVendedorOuGerente("Vendedor", cliente)
         except:
-            cliente.sendall("Erro".encode("utf-8"))
             print("Erro. Tente novamente")
+            cliente.sendall("Erro".encode("utf-8"))
             iniciarVendedorOuGerente("Vendedor", cliente)
     else:
-        cliente.sendall("Esse código de operação não existe ou você não tem acesso ao mesmo".encode("utf-8"))
+        cliente.sendall("ERRO_OP".encode("utf-8"))
         print("Esse código de operação não existe ou você não tem acesso ao mesmo. Tente outro por favor...")
         iniciarVendedorOuGerente("Vendedor", cliente)
 
